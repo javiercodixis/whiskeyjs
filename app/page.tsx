f0ede6112cdfc2511ff9afb7177ea5458500cce8
javiercodixis <jmoreno@codixis.com>
2023-10-09 11:58:05 +0300

    basic form implementation to be changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,75 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { Select, MenuItem, TextField, FormControl, InputLabel, Button, Box, Checkbox, FormControlLabel } from '@mui/material';
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  whiskey: string;
+  quantity: number;
+  address: string;
+  ageVerified: boolean;
+};
+
+export default function WhiskeyPurchaseForm() {
+  const { register, handleSubmit, control } = useForm<FormValues>({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      whiskey: 'Whiskey Option 1',
+      quantity: 1,
+      address: '',
+      ageVerified: false,
+    },
+  });
+
+  const whiskeyOptions = ['Whiskey Option 1', 'Whiskey Option 2', 'Whiskey Option 3'];
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off" sx={{ display: 'flex', flexDirection: 'column', width: '50%', margin: '0 auto' }}>
+      <TextField {...register('firstName')} label="First Name" required variant="outlined" sx={{ marginBottom: 2, width: '50%' }} />
+      <TextField {...register('lastName')} label="Last Name" required variant="outlined" sx={{ marginBottom: 2, width: '50%' }} />
+      <TextField {...register('email')} label="Email" required variant="outlined" sx={{ marginBottom: 2, width: '50%' }} />
+      <TextField {...register('phone')} label="Phone" required variant="outlined" sx={{ marginBottom: 2, width: '50%' }} />
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <FormControl variant="outlined" sx={{ marginBottom: 2, width: '50%' }}>
+        <InputLabel id="whiskey-label">Whiskey</InputLabel>
+        <Controller
+          name="whiskey"
+          control={control}
+          defaultValue={whiskeyOptions[0]}
+          render={({ field }) => (
+            <Select labelId="whiskey-label" label="Whiskey" {...field}>
+              {whiskeyOptions.map(option => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
         />
-      </div>
+      </FormControl>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <TextField {...register('quantity')} label="Quantity" required variant="outlined" type="number" sx={{ marginBottom: 2, width: '50%' }} />
+      <TextField {...register('address')} label="Address" required multiline rows={4} variant="outlined" sx={{ marginBottom: 2, width: '100%' }} />
+      
+      <FormControlLabel
+        control={<Checkbox {...register('ageVerified')} />}
+        label="I verify that I am over 21 years of age"
+        sx={{ marginBottom: 2 }}
+      />
+      
+      <Button type="submit" variant="contained" color="primary">Purchase</Button>
+    </Box>
+  );
 }
