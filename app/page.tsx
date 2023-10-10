ed320b363f013ee38ef1e231da26bb99a3afdb94
javiercodixis <jmoreno@codixis.com>
2023-10-10 12:34:36 +0300

    feedback applied

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,15 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
 import TextInput from '@/components/TextInput';
 import { FormValues } from '@/types';
 
-const whiskeyOptions = ['Whiskey Option 1', 'Whiskey Option 2', 'Whiskey Option 3'];
-
 const Page = () => {
-  const { handleSubmit, control } = useForm<FormValues>({
-    defaultValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phoneNumber: '',
-      whiskey: '',
-      quantity: 1,
-      address: '',
-      ageVerified: false
-    }
-  });
-  
+  const { handleSubmit, control } = useForm<FormValues>();
+
+
   const onSubmit = (data: FormValues) => {
     console.log(data);
   };
@@ -34,62 +23,59 @@ const Page = () => {
       onSubmit={handleSubmit(onSubmit)}
       noValidate
       autoComplete="off"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '50%',
-        margin: '0 auto'
-      }}
+      sx={{ display: 'flex', flexDirection: 'column', width: '50%', margin: '0 auto' }}
     >
-      <Box
-        sx={{ display: 'flex', justifyContent: 'space-between' }}
-      >
-        <TextInput
-          name="firstName"
-          label="First Name"
-          control={control}
-          rules={{ required: 'First Name is required' }}
-        />
-        <TextInput
-          name="lastName"
-          label="Last Name"
-          control={control}
-          rules={{ required: 'Last Name is required' }}
-        />
-      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextInput
+            name="firstName"
+            label="First Name"
+            fullWidth
+            control={control}
+            rules={{ required: 'First Name is required' }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextInput
+            name="lastName"
+            label="Last Name"
+            fullWidth
+            control={control}
+            rules={{ required: 'Last Name is required' }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextInput
+            name="email"
+            label="Email"
+            fullWidth
+            control={control}
+            rules={{ required: 'Email is required' }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextInput
+            name="phoneNumber"
+            label="Phone Number"
+            fullWidth
+            control={control}
+            rules={{ required: 'Phone Number is required' }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextInput
+            name="address"
+            label="Address"
+            fullWidth
+            control={control}
+            multiline
+            rows={4}
+            rules={{ required: 'Address is required' }}
+          />
+        </Grid>
+      </Grid>
       <Divider sx={{ my: 1, borderBottomWidth: '2px' }} />
-      <Box
-        sx={{ display: 'flex', justifyContent: 'space-between' }}
-      >
-        <TextInput
-          name="email"
-          label="Email"
-          control={control}
-          rules={{ required: 'Email is required' }}
-        />
-        <TextInput
-          name="phoneNumber"
-          label="Phone Number"
-          control={control}
-          rules={{ required: 'Phone Number is required' }}
-        />
-      </Box>
-      <Divider sx={{ my: 1, borderBottomWidth: '2px' }} />
-      <TextInput
-        fullWidth
-        name="address"
-        label="Address"
-        control={control}
-        multiline
-        rows={4}
-        rules={{ required: 'Address is required' }}
-      />
-      <Divider sx={{ my: 1, borderBottomWidth: '2px' }} />
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-      >
+      <Button type="submit" variant="contained" color="primary">
         Submit
       </Button>
     </Box>
--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -3,32 +3,31 @@ import { Controller, Control } from 'react-hook-form';
 import TextField, { TextFieldProps } from '@mui/material/TextField';
 import { FormValues } from '@/types';
 
-type TextInputProps = TextFieldProps &  {
+type TextInputProps = TextFieldProps & {
   name: keyof FormValues;
-  label: string;
   control: Control<FormValues>;
   rules?: object;
-  fullWidth?: boolean;
-  halfWidth?: boolean;
-}
-
-const TextInput: React.FC<TextInputProps> = ({ name, label, control, rules, fullWidth, halfWidth }) => {
-  return (
-    <Controller
-      name={name}
-      control={control}
-      rules={rules}
-      render={({ field, fieldState }) => (
-        <TextField
-          {...field}
-          fullWidth={fullWidth || !halfWidth}
-          label={label} 
-          error={!!fieldState.error}
-          helperText={fieldState.error ? fieldState.error.message : null}
-        />
-      )}
-    />
-  );
 };
+
+const TextInput = ({
+  name,
+  control,
+  rules,
+  ...rest
+}: TextInputProps) => (
+  <Controller
+    name={name}
+    control={control}
+    rules={rules}
+    render={({ field, fieldState }) => (
+      <TextField
+        {...field}
+        {...rest}
+        error={!!fieldState.error}
+        helperText={fieldState.error ? fieldState.error.message : null}
+      />
+    )}
+  />
+);
 
 export default TextInput;
