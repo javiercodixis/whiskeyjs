808390217bf3376d304054c8b1c6eb9f0869aee0
javiercodixis <jmoreno@codixis.com>
2023-10-16 12:21:29 +0300

    type set to number

--- components/InputNumber.tsx
+++ components/InputNumber.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Controller, Control } from 'react-hook-form';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -22,22 +22,6 @@ const InputNumber = ({
   label,
   ...rest
 }: InputNumberProps) => {
-  const [value, setValue] = useState<number | string>(defaultValue || 1);
-
-  const handleIncrement = (onChange: (value: number | string) => void) => {
-    let newValue = Number(value) + 1;
-    if (newValue > 99) newValue = 99;
-    setValue(newValue);
-    onChange(newValue);
-  };
-    
-  const handleDecrement = (onChange: (value: number | string) => void) => {
-    let newValue = Number(value) - 1;
-    if (newValue < 1) newValue = 1;
-    setValue(newValue);
-    onChange(newValue);
-  };
-
   return (
     <Controller
       name={name}
@@ -45,60 +29,72 @@ const InputNumber = ({
       rules={rules}
       defaultValue={defaultValue}
       render={({ field }) => (
-        <Box>
-          <Box
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontFamily: 'IBM Plex Sans, sans-serif',
+            fontWeight: 400,
+          }}
+        >
+          <Button
+            onClick={() => {
+              let newValue = Number(field.value) - 1;
+              if (newValue < 1) newValue = 1;
+              field.onChange(newValue);
+            }}
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              fontFamily: 'IBM Plex Sans, sans-serif',
-              fontWeight: 400,
+              borderRadius: '999px',
+              color: (theme) =>
+                theme.palette.mode === 'dark' ? '#66b2ff' : '#0072e5',
+              '&:hover': {
+                background: (theme) =>
+                  theme.palette.mode === 'dark' ? '#004c99' : '#daecff',
+              },
             }}
           >
-            <Button
-              onClick={() => handleDecrement(field.onChange)}
-              sx={{
-                borderRadius: '999px',
-                color: (theme) =>
-                  theme.palette.mode === 'dark' ? '#66b2ff' : '#0072e5',
-                '&:hover': {
-                  background: (theme) =>
-                    theme.palette.mode === 'dark' ? '#004c99' : '#daecff',
-                },
-              }}
-            >
-              <RemoveIcon />
-            </Button>
-            <TextField
-              value={field.value}
-              onChange={(e) => {
-                setValue(e.target.value);
-                field.onChange(e);
-              }}
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              sx={{
-                margin: '0 10px',
-                width: '4rem',
-              }}
-            />
-            <Button
-              onClick={() => handleIncrement(field.onChange)}
-              sx={{
-                borderRadius: '999px',
-                color: (theme) =>
-                  theme.palette.mode === 'dark' ? '#66b2ff' : '#0072e5',
-                '&:hover': {
-                  background: (theme) =>
-                    theme.palette.mode === 'dark' ? '#004c99' : '#daecff',
-                },
-              }}
-            >
-              <AddIcon />
-            </Button>
-          </Box>
+            <RemoveIcon />
+          </Button>
+
+          <TextField
+            {...field}
+            type="number"
+            variant="outlined"
+            sx={{
+              margin: '0 10px',
+              width: '4rem',
+            }}
+            inputProps={{ 
+              min: 1, 
+              max: 99, 
+              step: 1 
+            }}
+            value={typeof field.value === 'number' ? field.value : ''}
+            onChange={(e) => {
+              const newValue = parseInt(e.target.value, 10);
+              field.onChange(newValue);
+            }}
+          />
+
+          <Button
+            onClick={() => {
+              let newValue = Number(field.value) + 1;
+              if (newValue > 99) newValue = 99;
+              field.onChange(newValue);
+            }}
+            sx={{
+              borderRadius: '999px',
+              color: (theme) =>
+                theme.palette.mode === 'dark' ? '#66b2ff' : '#0072e5',
+              '&:hover': {
+                background: (theme) =>
+                  theme.palette.mode === 'dark' ? '#004c99' : '#daecff',
+              },
+            }}
+          >
+            <AddIcon />
+          </Button>
         </Box>
       )}
     />
